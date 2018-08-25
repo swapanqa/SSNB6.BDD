da3bd2f25f8a009a9eed13218d28a431352824ec
I. Ivaan <iivaan@shiftedtech.com>
2018-08-25 14:29:57 -0400

    Cloud exec

--- src/main/java/com/shiftedtech/framework/DriverFactory.java
+++ src/main/java/com/shiftedtech/framework/DriverFactory.java
@@ -21,6 +21,11 @@ public class DriverFactory {
 	private ThreadLocal<WebDriver> driverCollection = new ThreadLocal<WebDriver>();
 	
 	private static String LOCAL_GRID_URL = "http://10.10.20.70:4444/wd/hub";
+	
+	public static final String CLOUD_USERNAME = "shehlaiftekhar1";
+	public static final String CLOUD_AUTOMATE_KEY = "ZpoB7yapem3YDyLRnfyD";
+	public static final String CLOUD_URL = "https://" + CLOUD_USERNAME + ":" + CLOUD_AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
  
 	
 	private static DriverFactory instance = null;
@@ -81,6 +86,71 @@ public class DriverFactory {
 		         caps.setBrowserName("chrome");
 		         try {
 		        	 WebDriver driver = new RemoteWebDriver(new URL(LOCAL_GRID_URL),caps);
+		             instance.driverCollection.set(driver);
+		            } catch (MalformedURLException e) {
+		                e.printStackTrace();
+		            }
+			}
+			else if(browser.toUpperCase().contentEquals("CLOUD-CH")) {
+				DesiredCapabilities caps = new DesiredCapabilities();
+			    caps.setCapability("browser", "Chrome");
+			    caps.setCapability("browser_version", "69.0 beta");
+			    caps.setCapability("os", "Windows");
+			    caps.setCapability("os_version", "7");
+			    caps.setCapability("resolution", "1920x1080");
+			    
+		         try {
+		        	 WebDriver driver = new RemoteWebDriver(new URL(CLOUD_URL),caps);
+		             instance.driverCollection.set(driver);
+		            } catch (MalformedURLException e) {
+		                e.printStackTrace();
+		            }
+			}
+			else if(browser.toUpperCase().contentEquals("CLOUD-CH-MAC")) {
+				DesiredCapabilities caps = new DesiredCapabilities();
+			    caps.setCapability("browser", "Chrome");
+			    caps.setCapability("browser_version", "65.0");
+			    caps.setCapability("os", "OS X");
+			    caps.setCapability("os_version", "High Sierra");
+			    caps.setCapability("resolution", "1920x1080");
+			    caps.setCapability("browserstack.debug","true");
+			    
+		         try {
+		        	 WebDriver driver = new RemoteWebDriver(new URL(CLOUD_URL),caps);
+		             instance.driverCollection.set(driver);
+		            } catch (MalformedURLException e) {
+		                e.printStackTrace();
+		            }
+			}
+			else if(browser.toUpperCase().contentEquals("CLOUD-SAF-MAC")) {
+				DesiredCapabilities caps = new DesiredCapabilities();
+			    caps.setCapability("browser", "Safari");
+			    caps.setCapability("browser_version", "10.1");
+			    caps.setCapability("os", "OS X");
+			    caps.setCapability("os_version", "Sierra");
+			    caps.setCapability("resolution", "1920x1080");
+
+			    
+		         try {
+		        	 WebDriver driver = new RemoteWebDriver(new URL(CLOUD_URL),caps);
+		             instance.driverCollection.set(driver);
+		            } catch (MalformedURLException e) {
+		                e.printStackTrace();
+		            }
+			}
+			else if(browser.toUpperCase().contentEquals("CLOUD")) {
+				PropertyReaderService property = PropertyReaderService.getInstance();
+				
+				DesiredCapabilities caps = new DesiredCapabilities();
+			    caps.setCapability("browser", property.getProperty("CLOUD_BROWSER"));
+			    caps.setCapability("browser_version", property.getProperty("CLOUD_BROWSER_VERSION"));
+			    caps.setCapability("os", property.getProperty("CLOUD_OS"));
+			    caps.setCapability("os_version", property.getProperty("CLOUD_OS_VERSION"));
+			    caps.setCapability("resolution", property.getProperty("CLOUD_RESULATION"));
+			    caps.setCapability("browserstack.debug",property.getProperty("CLOUD_DEBUG"));
+			    
+		         try {
+		        	 WebDriver driver = new RemoteWebDriver(new URL(CLOUD_URL),caps);
 		             instance.driverCollection.set(driver);
 		            } catch (MalformedURLException e) {
 		                e.printStackTrace();
